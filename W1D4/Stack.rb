e782f89a4dee05599d645b6b4b58832a6bc710d4
MarianaShebanova <mariana.shebanova@gmail.com>
2019-01-10 20:41:16 -0800

    Creating Map, Stack and Queue

---
+++ W1D4/Stack.rb
@@ -0,0 +1,20 @@
+class Stack
+  attr_reader :array
+
+  def initialize
+    @array = []
+  end
+
+  def push(el)
+    array.push(el)
+    el
+  end
+
+  def pop
+    array.pop
+  end
+
+  def peek
+    array[-1]
+  end
+end
